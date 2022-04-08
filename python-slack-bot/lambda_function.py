6b1d6db29c8807a88940396b2596aa1b5cbb3a3d
Tyler Pettitt <typettitt@gmail.com>
2022-04-07 19:37:42 -0500

    formatting

--- python-slack-bot/lambda_function.py
+++ python-slack-bot/lambda_function.py
@@ -10,32 +10,44 @@ from config import Config
 
 config = Config()
 
-kms = boto3.client('kms')
+kms = boto3.client("kms")
 expected_token = kms.decrypt(
     CiphertextBlob=b64decode(config.ENCRYPTED_EXPECTED_TOKEN),
-    EncryptionContext={'LambdaFunctionName': os.environ['AWS_LAMBDA_FUNCTION_NAME']}
-)['Plaintext'].decode('utf-8')
+    EncryptionContext={"LambdaFunctionName": os.environ["AWS_LAMBDA_FUNCTION_NAME"]},
+)["Plaintext"].decode("utf-8")
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 def respond(err, res=None):
-    return {
-        'statusCode': '400' if err else '200',
-        'body': err.message if err else json.dumps(res).encode('utf8'),
-        'headers': {
-            'Content-Type': 'application/json',
-        },
-    }
+    if err:
+        return {
+            "statusCode": "400",
+            "body": err.message,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+        }
+    else:
+        return {
+            "statusCode": "200",
+            "body": "",
+            "headers": {
+                "Content-Type": "application/json",
+            },
+        }
+
 
 def lambda_handler(event, context):
-    params = parse_qs(event['body'])
-    token = params['token'][0]
+    logger.info(context)
+    params = parse_qs(event["body"])
+    token = params["token"][0]
     if token != expected_token:
         logger.error("Request token (%s) does not match expected", token)
-        return respond(Exception('Invalid request token'))
-    
+        return respond(Exception("Invalid request token"))
+
     weather = Weather(params)
     res = weather.process()
     logger.info(res)
-    return respond(None, "Processing Complete")
+    return respond(None)
